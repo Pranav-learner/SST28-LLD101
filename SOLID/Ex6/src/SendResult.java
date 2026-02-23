f01bfeece9a1ffb9956a8bb512aa765cac96e61e
Pranav Gupta <pranavgupta32006@gmail.com>
2026-02-23 10:32:34 +0530

    Ex 6 Done

---
+++ SOLID/Ex6/src/SendResult.java
@@ -0,0 +1,10 @@
+
+public class SendResult {
+    public final boolean success;
+    public final String message;
+
+    public SendResult(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+}
