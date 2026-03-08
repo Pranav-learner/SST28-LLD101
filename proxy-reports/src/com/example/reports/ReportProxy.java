f75ac1ea8436c765063da08810b6346d3dfa0681
Pranav Gupta <pranavgupta32006@gmail.com>
2026-03-08 15:56:12 +0530

    Assignement Done

--- proxy-reports/src/com/example/reports/ReportProxy.java
+++ proxy-reports/src/com/example/reports/ReportProxy.java
@@ -8,6 +8,7 @@ package com.example.reports;
  * - caching of RealReport within the same proxy
  */
 public class ReportProxy implements Report {
+    private RealReport realReport;   
 
     private final String reportId;
     private final String title;
@@ -24,7 +25,14 @@ public class ReportProxy implements Report {
     public void display(User user) {
         // Starter placeholder: intentionally incorrect.
         // Students should remove direct real loading on every call.
-        RealReport report = new RealReport(reportId, title, classification);
-        report.display(user);
+        boolean access = accessControl.canAccess(user,classification);
+        if(!access){
+            System.out.println("Access Denied");
+            return;
+        }
+        if(realReport == null){
+        realReport = new RealReport(reportId, title, classification);
+        }
+        realReport.display(user);
     }
 }
